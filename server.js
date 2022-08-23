e33bf88e5e59b3c7544f13afa09311fc2df59803
Sukhdev Gill <26435947+sukhdevgill@users.noreply.github.com>
2022-08-23 13:23:33 +0100

    Add env port

--- server.js
+++ server.js
@@ -331,6 +331,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log('Server is listening on port', port));
